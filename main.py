37ee1c40d14ba637f721757cc7759c29f34a51a7
NiltonSV <102979433+NiltonSV@users.noreply.github.com>
2022-04-17 19:04:48 -0500

    Update main.py
    En el código se agregó una forma de crear una carpeta donde almacenaremos las imágenes que tomaremos en una reunión.

--- main.py
+++ main.py
@@ -1,13 +1,32 @@
-from PIL import ImageGrab
 import keyboard
-x=2
+import os
 
-print(keyboard.read_key())
+from datetime import datetime
+from PIL import ImageGrab
 
-if keyboard.read_key() == "ctrl":
-    save_path = '/Users/PC/Desktop/Screenshot_RAS_UNI/ss_'+str(x)+'.png'
-    ImageGrab.grab().save(save_path)
-  
+x = 1
+imagenes = [] # Lista para guardar los nombres de las imágenes
+
+#ALMACENAMOS EN UNA VARIABLE LA FECHA Y HORA DE LA REUNIÓN
+Fecha_hora = datetime.now().strftime('%Y-%m-%d %H;%M;%S')
+
+#CREAMOS LA CARPETA DONDE GUARDAREMOS LAS IMÁGENES DE LA REUNIÓN
+if os.path.exists('/Users/PC/Desktop/Screenshot_RAS_UNI/'+Fecha_hora):
+   print("La carpeta ya existe")
 else:
-    print("Nada")
+   os.mkdir('/Users/PC/Desktop/Screenshot_RAS_UNI/'+Fecha_hora)
 
+while True:
+   # Con keyboard.read_key() leeremos la tecla que se presiona
+    if keyboard.read_key() == "imp pant":
+        # Guardamos en una variable la ruta y nombre de la imagen
+        save_path = '/Users/PC/Desktop/Screenshot_RAS_UNI/'+Fecha_hora+'/ss_'+str(x)+'.png'
+        ImageGrab.grab().save(save_path)  
+        imagenes.append("ss_"+str(x)+".png") # Agregamos el nombre a la lista imagenes
+        x += 1
+        print("Imagen guardada") # Referencia para ver si la imagen se tomó
+
+    elif keyboard.read_key() == "ctrl": # Salimos del while cuando presionamos la tecla ctrl
+        break
+
+print(imagenes)
